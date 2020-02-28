5136878d7370b77be4b564321b30504600953a0f
alfonso royo jordan <alfonsoroyo@hotmail.com>
2020-02-28 18:35:44 +0100

    Add project files.

---
+++ P1_Class/cPrincipal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_Class
+{
+    class cPrincipal
+    {
+        static void Main(string[] args)
+        {
+            //contructor de una nueva clase
+
+            cVirus covid19 = new cVirus();
+            covid19.contagiar();
+            covid19.informeStatvirus();
+            for(int i=0; i<100; i++)
+            {
+                covid19.contagiar();
+                covid19.informeStatvirus();
+
+            }
+
+            covid19.informeStatvirus();
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ P1_Class/cVirus.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_Class
+{
+    class cVirus
+    {
+        public string tipus;
+        private int Peligrosidad;
+        private int taxaCuracion;
+        private int numero_infectats;
+      
+        //Metode constructor
+        public cVirus()
+        {
+            this.tipus = "corona";
+            this.taxaCuracion = 2;
+            this.numero_infectats = 0;
+            this.Peligrosidad = 0;
+
+
+
+        }
+
+//metodo publico
+         public void contagiar()
+        {
+
+            this.numero_infectats++;
+
+            this.Peligrosidad = numero_infectats / 10;
+
+        }public void informeStatvirus()
+        {
+
+            Console.Write("\n Tipus:" + this.tipus);
+            Console.Write("\n Numero infectat:" + this.numero_infectats);
+            Console.Write("\n perillositat:" + this.Peligrosidad);
+        }
+
+
+    }
+}
